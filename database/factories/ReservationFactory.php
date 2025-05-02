06fc969f39f72a38de09e9d83c5c47ddb394d1f4
Ezzeldien-M-Abdalaziz <ezzeldien.m.abdalziz@gmail.com>
2025-05-02 08:21:05 +0300

    migrations , factories , seeds , models

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->first()->id ? : User::factory(),
+            'service_id' => Service::inRandomOrder()->first()->id ? : Service::factory(),
+            'date_time' => $this->faker->dateTime('+1 days', '+1 month'),
+            'status' => $this->faker->randomElement(['pending', 'confirmed', 'cancelled']),
+        ];
+    }
+}
---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $services = [
+            'Business Consultation',
+            'Life Coaching Session',
+            'Laptop Repair',
+            'Home Cleaning Service',
+            'Personal Fitness Training',
+            'Tax Preparation Assistance',
+            'Therapy Session',
+            'Resume Review & Career Advice',
+            'Interior Design Consultation',
+        ];
+        foreach ($services as $service) {
+            Service::create([
+                'name' => $service,
+                'description' => fake()->sentence(nbWords: 50),
+                'price' => rand(100, 1000),
+                'duration' => rand(1, 30) . ' days',
+            ]);
+        }
+    }
+}
